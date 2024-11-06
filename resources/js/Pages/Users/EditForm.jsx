4e9c5a3265ecd663226c64811469a677b6b8ac85
Ahmad Najmi <122844294+ahmadnajmi90@users.noreply.github.com>
2024-11-06 10:17:18 +0800

    update title

--- resources/js/Pages/Users/EditForm.jsx
+++ resources/js/Pages/Users/EditForm.jsx
@@ -18,7 +18,7 @@ function EditForm({userDetails}) {
 
 <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
   <div className="mx-auto max-w-lg text-center">
-    <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+    <h1 className="text-2xl font-bold sm:text-3xl">Get  today!</h1>
 
     <p className="mt-4 text-gray-500">
       Register a new User here
